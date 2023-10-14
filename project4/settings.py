00316cd90f0e426ee2b274589520c9176af55bee
Vallala Rishwanth <115866700+rishwanthvallala@users.noreply.github.com>
2023-10-14 18:14:37 +0530

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -128,4 +128,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "vallalarishwanth@gmail.com"
-EMAIL_HOST_PASSWORD = 'axno hzao rbai cika'
+EMAIL_HOST_PASSWORD = 'xxxx xxxx xxxx xxxx'
